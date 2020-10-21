57d1b6332e90e21c1809956462fa0e85edbfa2dc
sis-flag <57098249+sis-flag@users.noreply.github.com>
2020-10-21 23:02:17 +0800

    create on 2020-10-21

---
+++ code/plot_result.py
@@ -0,0 +1,25 @@
+# !python3
+# coding: utf-8
+# author: sis-flag
+
+import os
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+with open(os.path.join("..", "exp", "sin_N", "data.pkl"), "rb") as f:
+    R1 = pickle.load(f)
+with open(os.path.join("..", "exp", "sin_M", "data.pkl"), "rb") as f:
+    R2 = pickle.load(f)
+    
+plt.figure()
+plt.semilogy(R1["error"], label="Normal")
+plt.semilogy(R2["error"], label="Mscale")
+plt.show()
+    
+#for k in [0, 500, 1000, 2000, 5000]:
+#    plt.figure()
+#    plt.plot(R2["x_samp"], R2["u_samp_"+str(k)], 'r--')
+#    plt.plot(R2["x_samp"], R2["u_samp_true"], 'b-')
+#    plt.show()
